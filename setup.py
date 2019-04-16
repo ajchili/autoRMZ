1fbd4c45c226d1c93b6b9e7e171612a51fee181f
Kirin Patel <kirinpatel@gmail.com>
2019-04-16 17:15:28 -0400

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup (
+	name='autoRMZ',
+	version='0.0.1',
+	packages=[''],
+	url='https://github.com/ajchili/autoRMZ',
+	license='MIT',
+	author='ajchili (Kirin Patel)',
+	author_email='kirinpatel@gmail.com',
+	description='A TensorFlow application built to help detect meteors within spectrograms, inspired by Radio Meteor Zoo on zooniverse.org.'
+	requires=['opencv-python']
+)
