e164f8eaab865ee85c1ce84450c2685a030c0468
Kirin Patel <kirinpatel@gmail.com>
2019-04-16 21:27:02 -0400

    Setup cnn

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from distutils.core import setup
 setup (
 	name='autoRMZ',
 	version='0.0.1',
-	packages=[''],
+	packages=['tensorflow', 'numpy'],
 	url='https://github.com/ajchili/autoRMZ',
 	license='MIT',
 	author='ajchili (Kirin Patel)',
 	author_email='kirinpatel@gmail.com',
 	description='A TensorFlow application built to help detect meteors within spectrograms, inspired by Radio Meteor Zoo on zooniverse.org.'
-	requires=['']
 )
